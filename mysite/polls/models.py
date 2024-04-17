9877d05a8af9614cb57fd4b16d59f6ced047eccc
anuj8848 <anujgiri8848@gmail.com>
2024-04-17 23:26:55 +0545

    models created and linked

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text=models.CharField(max_length=300)
+    pub_data=models.DateTimeField("data published")
+    
+
+class Choice(models.Model):
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text=models.CharField(max_length=100)
+    votes=models.IntegerField(default=0)
+    
+    
